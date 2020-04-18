bac95321f5d1f258305a469d6b4f753e88ccb43f
Osmo Johannes Siitonen <osmsiito@eduouka.fi>
2020-04-18 13:12:15 +0300

    Add project files.

---
+++ GameServer2/GameLogic.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameServer2
+{
+    class GameLogic
+    {
+        public static void Update()
+        {
+            ThreadManager.UpdateMain();
+        }
+    }
+}
